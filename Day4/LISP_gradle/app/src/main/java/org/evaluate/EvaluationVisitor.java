78b3fd42695a9824c44700999ccadbf0c84f03db
itsKarthik17 <karthiksekar005@gmail.com>
2026-01-14 10:59:22 +0530

    updated

--- Day4/LISP_gradle/app/src/main/java/org/evaluate/EvaluationVisitor.java
+++ Day4/LISP_gradle/app/src/main/java/org/evaluate/EvaluationVisitor.java
@@ -38,7 +38,10 @@ public class EvaluationVisitor implements Visitor {
                         * (int) elements.get(2).accept(this);
 
             case ">":
-                return (int) elements.get(1).accept(this) > (int) elements.get(2).accept(this) ? 1 : 0;
+                if ((int) elements.get(1).accept(this) > (int) elements.get(2).accept(this)) {
+                    return 1;
+                }
+                return 0;
 
             case "define":
                 String var = ((SymbolNode) elements.get(1)).getName();
@@ -48,9 +51,11 @@ public class EvaluationVisitor implements Visitor {
 
             case "if":
                 int condition = (int) elements.get(1).accept(this);
-                return condition != 0
-                        ? elements.get(2).accept(this)
-                        : elements.get(3).accept(this);
+                if (condition != 0) {
+                    return elements.get(2).accept(this);
+                } else {
+                    return elements.get(3).accept(this);
+                }
 
             default:
                 throw new RuntimeException("Unknown operator: " + operator);
